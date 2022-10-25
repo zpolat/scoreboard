b5ea2a644364a99959fd79101029c725cf4c9b04
Z Polat <digi.zaf@gmail.com>
2022-10-25 17:24:20 +0200

    comments

--- index.js
+++ index.js
@@ -4,35 +4,35 @@ let guest = 0
 let sumHomeEl = document.getElementById("sumHome")
 let sumGuestEl = document.getElementById("sumGuest")
 
-function addOneHome() {
-   home += 1
-   sumHomeEl.textContent = home
-}
+// function addOneHome() {
+//    home += 1
+//    sumHomeEl.textContent = home
+// }
 
-function addTwoHome() {
-   home += 2
-   sumHomeEl.textContent = home
-}
+// function addTwoHome() {
+//    home += 2
+//    sumHomeEl.textContent = home
+// }
 
-function addThreeHome() {
-   home += 3
-   sumHomeEl.textContent = home
-}
+// function addThreeHome() {
+//    home += 3
+//    sumHomeEl.textContent = home
+// }
 
-function addOneGuest() {
-   guest += 1
-   sumGuestEl.textContent = guest
-}
+// function addOneGuest() {
+//    guest += 1
+//    sumGuestEl.textContent = guest
+// }
 
-function addTwoGuest() {
-   guest += 2
-   sumGuestEl.textContent = guest
-}
+// function addTwoGuest() {
+//    guest += 2
+//    sumGuestEl.textContent = guest
+// }
 
-function addThreeGuest() {
-   guest += 3
-   sumGuestEl.textContent = guest
-}
+// function addThreeGuest() {
+//    guest += 3
+//    sumGuestEl.textContent = guest
+// }
 
 function reset() {
    sumHomeEl.textContent = "-"
