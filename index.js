e59d3972cf02ace0a934429cc0b6eb8601b8c39b
Z Polat <digi.zaf@gmail.com>
2022-10-25 20:11:13 +0200

    fix2

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let home = 0
 let guest = 0
+let side = ""
 let sumHomeEl = document.getElementById("sumHome")
 let sumGuestEl = document.getElementById("sumGuest")
 
@@ -25,19 +26,3 @@ function addscore(score, side) {
       console.log("kaput")
    }
 }
-
-function addscoreswitch(score, side {
-   switch (a) {
-      case 3:
-        alert( 'Too small' );
-        break;
-      case 4:
-        alert( 'Exactly!' );
-        break;
-      case 5:
-        alert( 'Too big' );
-        break;
-      default:
-        alert( "I don't know such values" );
-    
-})
