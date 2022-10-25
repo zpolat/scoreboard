95407b83bccd03ad8a77b587f16a54e4adfae46e
Z Polat <digi.zaf@gmail.com>
2022-10-25 13:19:43 +0200

    reset button

--- index.js
+++ index.js
@@ -1,35 +1,40 @@
-let home = 0;
-let guest = 0;
+let home = 0
+let guest = 0
 
 let sumHomeEl = document.getElementById("sumHome")
 let sumGuestEl = document.getElementById("sumGuest")
 
 function addOneHome() {
-    home += 1
-    sumHomeEl.textContent = home
+   home += 1
+   sumHomeEl.textContent = home
 }
 
 function addTwoHome() {
-    home += 2
-    sumHomeEl.textContent = home
+   home += 2
+   sumHomeEl.textContent = home
 }
 
 function addThreeHome() {
-    home += 3
-    sumHomeEl.textContent = home
+   home += 3
+   sumHomeEl.textContent = home
 }
 
 function addOneGuest() {
-    home += 1
-    sumGuestEl.textContent = home
+   guest += 1
+   sumGuestEl.textContent = guest
 }
 
 function addTwoGuest() {
-    home += 2
-    sumGuestEl.textContent = home
+   guest += 2
+   sumGuestEl.textContent = guest
 }
 
 function addThreeGuest() {
-    home += 3
-    sumGuestEl.textContent = home
+   guest += 3
+   sumGuestEl.textContent = guest
+}
+
+function reset() {
+   sumHomeEl.textContent = 0
+   sumGuestEl.textContent = 0
 }
