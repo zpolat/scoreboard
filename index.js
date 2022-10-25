9ff7e67285e67cc7cb4ec089fbd97607949c0a8d
Z Polat <digi.zaf@gmail.com>
2022-10-25 20:04:37 +0200

    opgeruimd

--- index.js
+++ index.js
@@ -1,40 +1,7 @@
 let home = 0
 let guest = 0
-
-let score = ""
-
 let sumHomeEl = document.getElementById("sumHome")
 let sumGuestEl = document.getElementById("sumGuest")
-
-// function addOneHome() {
-//    home += 1
-//    sumHomeEl.textContent = home
-// }
-
-// function addTwoHome() {
-//    home += 2
-//    sumHomeEl.textContent = home
-// }
-
-// function addThreeHome() {
-//    home += 3
-//    sumHomeEl.textContent = home
-// }
-
-// function addOneGuest() {
-//    guest += 1
-//    sumGuestEl.textContent = guest
-// }
-
-// function addTwoGuest() {
-//    guest += 2
-//    sumGuestEl.textContent = guest
-// }
-
-// function addThreeGuest() {
-//    guest += 3
-//    sumGuestEl.textContent = guest
-// }
 
 function reset() {
    sumHomeEl.textContent = "-"
